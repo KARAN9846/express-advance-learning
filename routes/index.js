f3befd09c6f8188df00f4b29d1cda6c6dd1f264e
Karan Jaria <jaria.karan@gmail.com>
2024-04-09 23:49:27 +0530

    inserting mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/create', async function(req, res, next) {
+
+  const createduser = await userModel.create({
+    username:"karan9846",
+    age: 25,
+    name:"karan"
+  })
+  res.send(createduser);
 });
 
 module.exports = router;
