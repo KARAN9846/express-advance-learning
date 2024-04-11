54b7303561f350f80f4049dacae468dac1fcd2ae
Karan Jaria <jaria.karan@gmail.com>
2024-04-11 20:10:01 +0530

    adding cookie & session

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require("./users")
+const userModel = require("./users");
+const { Cookie } = require('express-session');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 
 // user creation
@@ -24,23 +25,78 @@ router.get('/', function(req, res, next) {
 
 // Read
 
-router.get('/user', async function(req, res) {
+// router.get('/user', async function(req, res) {
 
-  const users = await userModel.find({
-    username:"karan9846"
-  })
-  res.send(users);
-});
+//   const users = await userModel.find({
+//     username:"karan9846"
+//   })
+//   res.send(users);
+// });
+
 
 // delete
 
 
-router.get('/delete', async function(req, res) {
+// router.get('/delete', async function(req, res) {
 
-  const deleteuser = await userModel.findAndDelete({
-    username:"karan9846"
-  })
-  res.send(deleteuser);
-});
+//   const deleteuser = await userModel.findAndDelete({
+//     username:"karan9846"
+//   })
+//   res.send(deleteuser);
+// });
+
+
+
+
+
+// session
+
+
+// router.get("/", function (req,res) {
+//   req.session.ban = true;
+//   res.render("index")
+
+// })
+
+
+// router.get("/checkban", function (req,res) {
+//   if (req.session.ban === true) {
+//     res.send("you are banned")
+//   }
+//   else {
+//     res.send("not banned")
+//   }
+// })
+
+
+// router.get("/removeban", function (req,res) {
+//   req.session.destroy(function(err) {
+//     if (err) throw err;
+//     res.send("banned removed")
+//   })
+// })
+
+
+
+// Cookie
+
+
+router.get("/", function (req,res) {
+ res.cookie("age", 25)
+ res.render("index")
+
+})
+
+router.get("/read", function (req,res) {
+console.log(req.cookies.age);
+res.send("check")
+
+})
+
+router.get("/delete", function (req,res) {
+res.clearCookie("age")
+res.send("cleared cookie")
+
+})
 
 module.exports = router;
